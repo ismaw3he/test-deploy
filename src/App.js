b1f854c3fb818e359ca1ff85074cdd93c592c82e
Rauf Ismayilov <rauf.raymond110@gmail.com>
2021-03-26 21:16:35 +0400

    test 1.9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About/About'
+import { HashRouter, Route, Link } from "react-router-dom";
 // import Menu from './components/Menu/Menu'
 
 function App() {
     return (
-       <div>
-           <h1>yep HAHAHHAHAahahaHAHHAHA</h1>
-       <About/>
-      
-       </div>
+        <HashRouter basename='/test-deploy'>
+            <div>
+                <h1>yep HAHAHHAHAahahaHAHHAHA</h1>
+                <About />
+
+            </div>
+        </HashRouter>
     );
 }
 
